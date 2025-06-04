5cda94714aa3d7ad1374bc6b3681ba0df31cdf13
Oliver-Timmerlid <113107596+Oliver-Timmerlid@users.noreply.github.com>
2025-06-04 16:32:04 +0200

    Added model

---
+++ Model/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevNest.Model;
+
+internal class Item
+{
+    public string Name { get; set; }
+    public string[] Frameworks { get; set; }
+    public string Description { get; set; }
+    
+}
